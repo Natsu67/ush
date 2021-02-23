8e3ce60dcfc47cbbbd2860d4f08f1bd22e50327b
Natsu67 <ashpigunov@c1r7p8.khpi.ucode-connect.study>
2021-02-23 16:06:58 +0200

    some fixes

--- src/parse_get_process_output.c
+++ src/parse_get_process_output.c
@@ -1,9 +1,9 @@
 #include "../inc/ush.h"
 
-static void end_reading(pid_t pid, int *pipe) {
+static void end_reading(pid_t pid, int *pipe_p) {
     int status;
     
-    close(pipe[0]);
+    close(pipe_p[0]);
     waitpid(pid, &status, WUNTRACED);
     tcsetpgrp(0, getpid());
     tcsetpgrp(1, getpid());
@@ -11,32 +11,32 @@ static void end_reading(pid_t pid, int *pipe) {
     errno = 0;
 }
 
-static char *read_output(pid_t pid, int *pipe) {
+static char *read_output(pid_t pid, int *pipe_p) {
     int read_bytes = 0;
     char buf[BUFSIZ + 1] = "";
     char *temp = NULL;
     char *ret = calloc(1, 1);
 
-    close(pipe[1]);
-    for (read_bytes = read(pipe[0], buf, BUFSIZ); read_bytes > 0;
-        read_bytes = read(pipe[0], buf, BUFSIZ)) {
+    close(pipe_p[1]);
+    for (read_bytes = read(pipe_p[0], buf, BUFSIZ); read_bytes > 0;
+        read_bytes = read(pipe_p[0], buf, BUFSIZ)) {
         temp = mx_strjoin(ret, buf);
         free(ret);
         ret = temp;
         memset(buf, 0, read_bytes);
     }
-    end_reading(pid, pipe);
+    end_reading(pid, pipe_p);
 
     return ret;
 }
 
 static char *process_output(char *str, int (*parse_p)(char *, t_ush *), t_ush *ush) {
     pid_t pid;
-    int pipe[2];
+    int pipe_p[2];
 
     if (!mx_check_parse(str)) return NULL;
 
-    pipe(pipe);
+    pipe(pipe_p);
     if ((pid = fork()) == -1) {
         perror("fork");
         return NULL; 
@@ -46,9 +46,9 @@ static char *process_output(char *str, int (*parse_p)(char *, t_ush *), t_ush *u
         tcsetpgrp(0, getpid());
         tcsetpgrp(1, getpid());
         setvbuf(stdout, NULL, _IONBF, 0);
-        close(pipe[0]);
-        dup2(pipe[1], 1);
-        close(pipe[1]);
+        close(pipe_p[0]);
+        dup2(pipe_p[1], 1);
+        close(pipe_p[1]);
         if (parse_p(str, ush) == -1) {
             fprintf(stderr, MX_ERR_PARSE_CMDSBN);
             exit(1);
@@ -56,7 +56,7 @@ static char *process_output(char *str, int (*parse_p)(char *, t_ush *), t_ush *u
         exit(0);
     }
 
-    return read_output(pid, pipe);
+    return read_output(pid, pipe_p);
 }
 
 char *mx_get_subst_outputs(char *str, t_ush *ush) {
