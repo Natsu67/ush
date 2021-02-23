e4b8a13780e629b9a0b746ccffbd1a2245f8d1f0
Ilya Razumeyko <irazumeyko@c1r6p8.khpi.ucode-connect.study>
2021-02-23 17:37:53 +0200

    pwd jobs and fg

--- src/ush_fg.c
+++ src/ush_fg.c
@@ -1,49 +1,31 @@
 #include "../inc/ush.h"
 
-static bool all_digits(char *args){
+static int is_digit(char *args){
     for (int i = 1; args[i]; i++) {
-        if (!mx_isdigit(args[i]))
-            return false;
+        if (!mx_isdigit(args[i])) return 0;
     }
-    return true;
+    return 1;
 }
 
-static bool pres_persent(char *args, t_jobs **jobs) {
-    if (mx_isalpha(args[1]) || args[1] == '-' 
-        || args[1] == '+' || args[1] == '%'|| args[1] == '\0') {
-        if (!mx_job_chars_find(args, jobs))
-            return false;
-    }
-    else if (all_digits(args)) {
-        if (!mx_job_num_find(args, jobs))
-            return false;
-    }
+static int is_symbol(char *args, t_jobs **jobs) {
+    if ((mx_isalpha(args[1]) || args[1] == '-' || args[1] == '+' || args[1] == '%'|| args[1] == '\0') && !mx_job_chars_find(args, jobs)) return 0;
+    else if (is_digit(args) && !mx_job_num_find(args, jobs)) return 0;
     else {
-        mx_not_found(args, "fg: job"); // любой другой знак
-        return false;
+        mx_not_found(args, "fg: job");
+        return 0;
     }
-    return true; 
+    return 1; 
 }
 
 int mx_ush_fg(char **args, t_jobs **jobs) {
     if (args[1] == NULL) {
-        if (mx_job_chars_find("%%", jobs))
-            return 1;
-        else 
-           return 0; 
+        if (mx_job_chars_find("%%", jobs)) return 1;
+        else return 0; 
     }
     for(int i = 1; args[i]; i++) {
-        if (mx_get_char_index(args[i], '%') == 0) {
-            if (!pres_persent(args[i], jobs))
-                return 1;
-        }
-        //букв команда без процента
-        else if (mx_isalpha(args[i][0])) {
-            if (!mx_job_chars_find(args[i], jobs))
-                return 1;
-        }
-        else 
-            return mx_not_found(args[i], "fg: job");
+        if (mx_get_char_index(args[i], '%') == 0 && !is_symbol(args[i], jobs)) return 1;
+        else if (mx_isalpha(args[i][0]) && !mx_job_chars_find(args[i], jobs)) return 1;
+        else return mx_not_found(args[i], "fg: job");
     }
     return 0;
 }
